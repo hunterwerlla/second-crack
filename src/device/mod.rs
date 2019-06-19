f4ed9b9385902efee98c271724dc3626dcabc774
Hunter Werlla <hunter@werlla.com>
2019-06-18 21:24:19 -0700

    working on hammering out the design

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Error;
+
 pub enum DataType {
     Temperature
 }
@@ -5,9 +7,14 @@ pub enum DataType {
 pub struct DataPoint{
     pub timestamp: std::time::SystemTime,
     pub value: f64,
-    pub data_type: DataType
+    pub data_type: DataType,
+    pub data_index: i32
 }
 
+pub trait DeviceContext {}
+
 pub trait Device {
-    fn poll() -> Vec<DataPoint>;
+    fn poll() -> Result<Vec<DataPoint>, Error>;
+    fn connect() -> Result<(), Error>;
+    fn get_sensor_string(data_type: DataType, offset: i32) -> Option<String>;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 mod device;
 
 use device::{Device, DataPoint, DataType};
+use std::fmt::Error;
 
 struct Whatever{
 }
 
 impl device::Device for Whatever {
-    fn poll() -> Vec<DataPoint>{
+    fn poll() -> Result<Vec<DataPoint>, Error>{
         let x = vec![DataPoint {
             timestamp: std::time::SystemTime::now(),
             value: 0.0,
-            data_type: DataType::Temperature
+            data_type: DataType::Temperature,
+            data_index: 0
         }];
-        return x
+        return Ok(x)
+    }
+
+    fn connect() -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn get_sensor_string(data_type: DataType, offset: i32) -> Option<String> {
+        match(data_type, offset) {
+            (DataType::Temperature, 0) => Some(String::from("Bean temp")),
+            _ => None
+        }
+
     }
 }
 
 fn main() {
-    println!("Hello, {}!", Whatever::poll()[0].value);
+    let poll = Whatever::poll().unwrap();
+    println!("Hello, {}!", poll.first().unwrap().value);
 }
