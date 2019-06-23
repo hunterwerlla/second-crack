624260235b52a4df70a76e04f54b078099f23538
Hunter Werlla <hunter@werlla.com>
2019-06-23 10:05:23 -0700

    final commit going in this direction

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,4 +1,6 @@
 // This layer represents control to the core and forms the common basis for the frontends,
 // This layer exists so there can be the possibility of a split front and back end
 
+// subscribe data
+// get current roast data
 // 
---
+++ src/control/neon/mod.rs
@@ -0,0 +1,6 @@
+use neon::prelude::*;
+use crate::device::device_scanner;
+
+fn scan_hardware(mut cx: FunctionContext) -> JsResult<JsNumber> {
+
+}
